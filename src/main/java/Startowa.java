50d8a76c22101b88213cc98050167f1604d0a687
WSIZ <teamWSIZ@users.noreply.github.com>
2016-05-06 14:40:18 +0200

    Update Startowa.java

--- src/main/java/Startowa.java
+++ src/main/java/Startowa.java
@@ -1,12 +1,16 @@
 import mypackage.BeanB;
+import mypackage.WeatherClient;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class Startowa {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring-config.xml");
-        BeanB beanB = ctx.getBean(BeanB.class);
-        beanB.useResources();
+
+        WeatherClient client = ctx.getBean(WeatherClient.class);
+        client.getCityForecastByZip("94304");
+
+
         ctx.close();
     }
 }
